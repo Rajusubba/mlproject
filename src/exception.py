3c5138aafcd1fb0e9be1d5d76d8e62a5eb4b10df
Raju Subba <subba.raju67@yahoo.com>
2025-03-25 12:25:43 -0400

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,19 +1,28 @@
+# filepath: /Users/rajusubba/MLProjects/src/exception.py
+import os
 import sys
-def error_message_detail(error, error_detail:sys):
-    _,_, exc_tb = error_detail.exc_info()
+
+# Set the working directory to the project root
+project_root = "/Users/rajusubba/MLProjects"
+if project_root not in sys.path:
+    sys.path.append(project_root)
+from src.logger import logging
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in  python script name [{0}] line number: [{1}] error message [{2}]".format()
-    file_name, exc_tb.tb_lineno, str(error)
+    error_message = "Error occurred in python script name [{0}] line number: [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
     return error_message
 
-    
-    
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail = error_detail)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
-   
+
+
     
