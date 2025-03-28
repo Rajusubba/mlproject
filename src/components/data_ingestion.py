6ae4057b53e5d7210f71113a2dabcdc4108ce514
Raju Subba <subba.raju67@yahoo.com>
2025-03-28 11:29:12 -0400

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,10 +3,10 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
+from src.components.data_transformation import DataTransformation
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformationConfig
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
@@ -41,5 +41,15 @@ class DataIngestion:
            raise CustomException(e,sys)
        
 if __name__ == '__main__':
-    obj=DataIngestion()
-    obj.initiative_data_ingestion()
+    try:
+        obj = DataIngestion()
+        train_data, test_data = obj.initiative_data_ingestion()
+
+        data_transformation = DataTransformation()
+        data_transformation.initiate_data_transformation(train_data, test_data)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    # Pass the train and test data paths to the function
+    
+    #print("✅ DataTransformation imported successfully")
