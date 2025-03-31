14ce0fd32f68b8ba07e930822b2a3048506f2cf0
Raju Subba <subba.raju67@yahoo.com>
2025-03-31 09:38:55 -0400

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ from src.components.data_transformation import DataTransformation
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
@@ -41,15 +44,17 @@ class DataIngestion:
            raise CustomException(e,sys)
        
 if __name__ == '__main__':
-    try:
-        obj = DataIngestion()
-        train_data, test_data = obj.initiative_data_ingestion()
+    
+    obj = DataIngestion()
+    train_data, test_data = obj.initiative_data_ingestion()
 
-        data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data, test_data)
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
 
-    except Exception as e:
-        raise CustomException(e, sys)
+    
     # Pass the train and test data paths to the function
     
     #print("✅ DataTransformation imported successfully")
